07ee5ff98fb2abb7d887171491069377bcb1f873
temomaglakelidze <154346509+temomaglakelidze@users.noreply.github.com>
2024-02-15 16:58:33 +0400

    approach section is done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ function toggleMenu() {
 
   var header = document.getElementById("header");
   header.classList.toggle("active-header");
+
+  var body = document.getElementById("body");
+  body.classList.toggle("overflow-hidden");
 }
 document.querySelector(".hamburger").addEventListener("click", (e) => {
   e.currentTarget.classList.toggle("is-active");
